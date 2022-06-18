6bb5aa00f120bcfce16c18ed0e9138d5db2a0c79
Utsav busa <utsavbusa@utsavs-MacBook-Air.local>
2022-06-18 22:42:43 +0530

    add gallery banner

--- js/main.js
+++ js/main.js
@@ -40,20 +40,8 @@ let slides=document.querySelectorAll('.slide-container');
         // autoplay
 
         setInterval(next,4000);
-
+        slides[0].addEventListener("swiped-right",next());
+        slides[0].addEventListener("swiped-left",prev());
         //  gallery section swiper js 
 
-let testimonial=document.querySelectorAll('.testimonials-content');
-let i=0;
 
-// next1 fuction
-
-function next1(){
-    testimonial[i].classList.remove('on');
-    document.getElementsByClassName("btn-1")[i].style.backgroundColor = "white";
-    i = (i + 1 ) % testimonial.length;
-    testimonial[index].classList.add('on');
-    document.getElementsByClassName("btn-1")[i].style.backgroundColor = "black";
-}
-
-setInterval(next1,4000);
